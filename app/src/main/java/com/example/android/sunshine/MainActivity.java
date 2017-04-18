d3e0b7dcc28a958f54532422f330171249bd3da7
Divya Gupta <divagupta2392@gmail.com>
2017-04-18 15:18:14 +0530

    Networking and Menu added

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,9 +15,20 @@
  */
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,28 +42,82 @@ public class MainActivity extends AppCompatActivity {
 
         //Use findViewById to get a reference to the weather display TextView
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
-
-        //Create an array of Strings that contain fake weather data
-        String[] dummyWeatherData = {
-                               "Today, May 17 - Clear - 17°C / 15°C",
-                                "Tomorrow - Cloudy - 19°C / 15°C",
-                                "Thursday - Rainy- 30°C / 11°C",
-                                "Friday - Thunderstorms - 21°C / 9°C",
-                                "Saturday - Thunderstorms - 16°C / 7°C",
-                                "Sunday - Rainy - 16°C / 8°C",
-                                "Monday - Partly Cloudy - 15°C / 10°C",
-                                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                                "Thu, May 26 - Stormy - 30°C / 11°C",
-                                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                                "Sat, May 28 - Meteors - 16°C / 7°C",
-                                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-                        };
-
-        for (String dummyWeatherDay : dummyWeatherData) {
-                        mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
-                    }
-    }
+        loadWeatherData();
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.forecast,menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_refresh)
+        {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadWeatherData()
+    {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>
+    {
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            /* If there's no zip code, there's nothing to look up. */
+            if (params.length == 0) {
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+         if(weatherData !=null)
+         {
+             /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
+             for(String weatherString : weatherData)
+                 mWeatherTextView.append((weatherString) + "\n\n\n");
+         }
+        }
+    }
+
+}
